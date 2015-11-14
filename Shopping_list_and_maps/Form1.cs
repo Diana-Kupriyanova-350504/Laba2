1537939c9d788d0e6d17482b6e90ab2df16c382f
Diana Kupriyanova 350504 <diankupriyanova@gmail.com>
2015-11-14 20:01:40 +0300

    jrganization of work shopping list

--- Shopping_list_and_maps/Form1.cs
+++ Shopping_list_and_maps/Form1.cs
@@ -29,50 +29,69 @@ namespace Shopping_list_and_maps
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            richTextBox1.Text = "";
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName);
+                richTextBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            SaveFileDialog svd = new SaveFileDialog();
+            svd.Filter = "Text files (*.txt)|*.txt";
+            if (svd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(svd.FileName);
+                sw.Write(richTextBox1.Text);
+                sw.Close();
+            }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
 
         private void undoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            richTextBox1.Undo();
         }
 
         private void reToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            richTextBox1.Redo();
         }
 
         private void cutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            richTextBox1.Cut();
         }
 
         private void copyToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            richTextBox1.Copy();
         }
 
         private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            richTextBox1.Paste();
         }
 
         private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
